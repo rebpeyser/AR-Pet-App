c577ec99de5a0c344f13141da0514745471f29d5
Rebecca Peyser <rebeccapeyser@rebeccas-macbook-pro.local>
2019-06-14 18:31:25 -0400

    completed function to add pets. finished app

--- AR Pet App/ViewController.swift
+++ AR Pet App/ViewController.swift
@@ -16,8 +16,15 @@ class ViewController: UIViewController {
     @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
         let tapLocation = sender.location(in: sceneView)
         let hitTestResults = sceneView.hitTest(tapLocation)
-        guard let node = hitTestResults.first?.node else { return }
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            if let hitTestResultsWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultsWithFeaturePoints.worldTransform.translation
+                addPet(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return }
         node.removeFromParentNode()
+        
     }
     
     let config = ARWorldTrackingConfiguration()
@@ -29,18 +36,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,   ARSCNDebugOptions.showWorldOrigin]
+        //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,   ARSCNDebugOptions.showWorldOrigin]
         sceneView.session.run(config)
         addPet()
     }
     
-    func addPet() {
+    func addPet(x: Float = 0, y: Float = 0, z: Float = -0.2) { //update in Part 5
         let petScene = SCNScene(named: "Animals/ShibaInu.scn")!
         petNode = petScene.rootNode.childNode(withName: petNodeName, recursively: true)
         petNode.scale = SCNVector3(0.1, 0.1, 0.1)
-        petNode.position = SCNVector3(0.1, 0.1, -0.1)
+        petNode.position = SCNVector3(x, y, z) //update in Part 5
         sceneView.scene.rootNode.addChildNode(petNode)
     }
 
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
 }
 
