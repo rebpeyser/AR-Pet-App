ecc6bd33f096a581f7023c7562cb9ee4eabaa519
Rebecca Peyser <rebeccapeyser@rebeccas-macbook-pro.local>
2019-06-14 18:11:52 -0400

    Added AR capabilities, and built remove pet function

--- AR Pet App/ViewController.swift
+++ AR Pet App/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else { return }
+        node.removeFromParentNode()
+    }
+    
+    let config = ARWorldTrackingConfiguration()
+    
+    var petNode: SCNNode!
+    let petNodeName = "shiba"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,   ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(config)
+        addPet()
     }
-
+    
+    func addPet() {
+        let petScene = SCNScene(named: "Animals/ShibaInu.scn")!
+        petNode = petScene.rootNode.childNode(withName: petNodeName, recursively: true)
+        petNode.scale = SCNVector3(0.1, 0.1, 0.1)
+        petNode.position = SCNVector3(0.1, 0.1, -0.1)
+        sceneView.scene.rootNode.addChildNode(petNode)
+    }
 
 }
 
